dd471922fe2cb729ee0c3a7877eb10ec6740f77e
Utkarsh@0011gmail.com <tiwariutkarsh0011@gmail.com>
2026-07-23 16:15:05 +0530

    with comments basics

--- basics/variables.cpp
+++ basics/variables.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-    int x=5;
-    cout<<x<<endl;
+    int x=5; // asssigning value 5 to x
+    cout<<x<<endl; // output
     x=x/5;
     cout<<x<<endl;
     x=x+20;
-    cout<<x<<"\n";
+    cout<<x<<"\n"; // new line
     x+=5;
     cout<<x<<endl;
     x=x*0;
