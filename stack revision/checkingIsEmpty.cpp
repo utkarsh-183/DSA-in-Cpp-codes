4def337e631f2b01a3f16f39b2d33fc86f252891
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-08-26 00:07:52 +0530

    checking stack is empty or not

---
+++ stack revision/checkingIsEmpty.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<int>st;
+    if(st.empty()){
+        cout<<"stack is empty"<<endl;
+    }
+    st.push(20);
+    if(!st.empty()){
+        cout<<"Stack is not empty and the top element is : "<<st.top()<<endl;
+    }
+    else{
+        cout<<"stack is empty"<<endl;
+    }
+}
