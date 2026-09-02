2d60092f5ad5c589d09961381b3dcc8deac43091
Utkarsh <tiwariutkarsh0011@gmail.com>
2026-09-02 14:41:14 +0530

    Passing object to a function

---
+++ oops in c++/passingOfObjectToFunction.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Example{
+public:
+    int a;
+    void add(Example E){
+        a = a+E.a;
+    }
+};
+
+int main(){
+    Example E1, E2;
+    E1.a = 50;
+    E2.a = 100;
+
+    cout<<"Initial Values"<<endl;
+    cout<<"Value of object 1: "<<E1.a<<endl<< "object 2: " <<E2.a<<endl;
+
+    E2.add(E1);
+
+    cout<<"new values :"<<endl;
+    cout<<"Value of object 1:"<<E1.a<<endl<<"object 2: "<<E2.a<<endl;
+
+    return 0;
+}
